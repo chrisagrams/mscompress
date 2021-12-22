11f9b9b1fe560ed00035c36d97bff7a4fc234113
Chris Grams <chrisagrams@gmail.com>
2021-12-22 17:24:41 -0600

    Added compress_xml

--- src/compress.c
+++ src/compress.c
@@ -19,7 +19,7 @@ alloc_cctx()
 }
 
 void*
-alloc_ztsd_buff(size_t src_len, size_t* buff_len)
+alloc_ztsd_cbuff(size_t src_len, size_t* buff_len)
 {
     size_t bound;
 
@@ -38,7 +38,7 @@ zstd_compress(ZSTD_CCtx* cctx, void* src_buff, size_t src_len, size_t* out_len, 
     size_t buff_len = 0;
 
 
-    out_buff = alloc_ztsd_buff(src_len, &buff_len);
+    out_buff = alloc_ztsd_cbuff(src_len, &buff_len);
 
     *out_len = ZSTD_compressCCtx(cctx, out_buff, buff_len, src_buff, src_len, compression_level);
 
@@ -46,4 +46,29 @@ zstd_compress(ZSTD_CCtx* cctx, void* src_buff, size_t src_len, size_t* out_len, 
         return NULL;
 
     return out_buff;
+}
+
+void *
+compress_xml(char* input_map, data_positions* dp)
+{
+    ZSTD_CCtx* czstd;
+    void* cmp;
+    size_t cmp_len = 0;
+
+    czstd = alloc_cctx();
+    
+    cmp = zstd_compress(czstd, input_map, dp->start_positions[0], &cmp_len, 1);
+
+    free(cmp);
+
+    cmp_len = 0;
+
+    size_t len;
+    
+    for (int i = 1; i < dp->total_spec * 2; i++)
+    {
+      len = dp->start_positions[i]-dp->end_positions[i-1];
+      cmp = zstd_compress(czstd, input_map+dp->end_positions[i-1], len, &cmp_len, 1);
+      free(cmp);
+    }
 }
