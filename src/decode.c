e2b6adaa8dd52de4c1882c367dce7f97f32ab2e5
Chris Grams <chrisagrams@gmail.com>
2021-12-23 15:10:53 -0600

    Commented decode.c

--- src/decode.c
+++ src/decode.c
@@ -8,6 +8,18 @@
 
 char*
 decode_base64(char* src, size_t src_len, size_t* out_len)
+/**
+ * @brief Wrapper function for base64 libary. 
+ * Allocates memory for base64_decode function call and takes care of errors.
+ * 
+ * @param src Pointer to beginning of base64 string (can be a substring within another string).
+ * 
+ * @param src_len Length of base64 string (not NULL terminated).
+ * 
+ * @param out_len Pass-by-reference return value of length of decoded string.
+ * 
+ * @return Decoded string.
+ */
 {
     int b64_ret;
 
@@ -30,6 +42,27 @@ int zlib_start_len = BUFSIZE;
 
 double*
 decode_zlib(char* src, size_t src_len, size_t* out_len)
+/**
+ * @brief Wrapper function for zlib library.
+ * To optimize for speed, this function dynamically adjusts the buffer size based on 
+ * the return value of the uncompress() function. If the buffer was too small for the zlib
+ * library, grow the buffer by the increment factor and retry. Since the decoded lengths between
+ * spectra are relatively similar within a .mzML file, this function will only hit a few "misses" 
+ * in the beginning of the .mzML parsing and stabilize as it progresses. Thus, this solution
+ * has a very minimal performance penalty while being memory efficient and scalable. The buffer
+ * size is adjusted in the global variable zlib_start_len. One alternative solution is to read
+ * all encodedLength accessions within the mzML file but this is a slow procedure do to the 
+ * string to integer conversion required. Another solution is to set a fixed buffer size but 
+ * this will be a waste of memory on smaller files. 
+ * 
+ * @param src zlib encoded string.
+ * 
+ * @param src_len Length of zlib encoded string (not NULL terminated).
+ * 
+ * @param out_len A pass-by-reference return value of the resulting decompressed string length.
+ * 
+ * @return 
+ */
 {
     int zlib_ret = 1;
     int increment = 2;
@@ -75,6 +108,22 @@ decode_zlib(char* src, size_t src_len, size_t* out_len)
 
 double*
 decode_binary(char* input_map, int start_position, int end_position, int compression_method, size_t* out_len)
+/**
+ * @brief A routine to decode a binary data element within a .mzML file.
+ * 
+ * @param input_map A mmap pointer to the .mzML file.
+ * 
+ * @param start_position The start position of base64 data within a <binary></binary> element within the .mzML document.
+ * 
+ * @param end_position The end position of base64 data within a <binary></binary> element within the .mzML document.
+ * 
+ * @param compression_method Given by data_format struct, the encoding of the binary data within the .mzML document given by preprocess.c:pattern_detect()
+ * 
+ * @param out_len A pass-by-reference return value of the length of the resulting decoded binary data.
+ * 
+ * @return
+ * 
+ */
 {
     int zlib_ret;
 
