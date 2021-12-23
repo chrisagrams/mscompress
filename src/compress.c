444d5ab4cdfb2976231dfcbb84830e4981c1df7a
Chris Grams <chrisagrams@gmail.com>
2021-12-23 16:12:09 -0600

    commented compress.c

--- src/compress.c
+++ src/compress.c
@@ -7,6 +7,12 @@
 
 ZSTD_CCtx*
 alloc_cctx()
+/**
+ * @brief Creates a ZSTD compression context and handles errors.
+ * 
+ * @return A ZSTD compression context on success. Exits on error.
+ * 
+ */
 {
     ZSTD_CCtx* cctx;
     cctx = ZSTD_createCCtx();
@@ -16,11 +22,34 @@ alloc_cctx()
         exit(1);
     }
     return cctx;
-
 }
+
+
+void
+dealloc_cctx(ZSTD_CCtx* cctx)
+/**
+ * @brief Deallocates a ZSTD compression context.
+ * 
+ * @param cctx A ZSTD compression context to free.
+ * 
+ */
+{
+    ZSTD_freeCCtx(cctx); /* Never fails. */
+}
+
 
 void*
 alloc_ztsd_cbuff(size_t src_len, size_t* buff_len)
+/**
+ * @brief Allocates a compression buffer for ZSTD with size based on ZSTD_compressBound.
+ * 
+ * @param scr_len Length of string to compress.
+ * 
+ * @param buff_len A pass-by-reference return value of the size of the buffer.
+ * 
+ * @return A buffer of size buff_len
+ * 
+ */
 {
     size_t bound;
 
@@ -34,6 +63,23 @@ alloc_ztsd_cbuff(size_t src_len, size_t* buff_len)
 
 void *
 zstd_compress(ZSTD_CCtx* cctx, void* src_buff, size_t src_len, size_t* out_len, int compression_level)
+/**
+ * @brief A wrapper function for ZSTD_compressCCtx.
+ * This function allows the reuse of a ZSTD compression context per thread to reduce resource consumption.
+ * This function takes care of allocating the proper buffer and handling errors.
+ * 
+ * @param cctx A ZSTD compression context allocated by alloc_cctx() (one per thread).
+ * 
+ * @param src_buff Source string to compress.
+ * 
+ * @param src_len Length of the source string.
+ * 
+ * @param out_len A pass-by-reference return value of the resulting compress string.
+ * 
+ * @param compresssion_level An integer (1-9) representing ZSTD compression strategy (see ZSTD documentation)
+ *
+ * @return A buffer with the compressed string on success, NULL on error.
+ */
 {
     void* out_buff;
     size_t buff_len = 0;
@@ -48,8 +94,17 @@ zstd_compress(ZSTD_CCtx* cctx, void* src_buff, size_t src_len, size_t* out_len, 
     return out_buff;
 }
 
+
 cmp_buff_t*
 alloc_cmp_buff(int allocation)
+/**
+ * @brief Allocates a cmp_buff_t struct to store compressed blocks (cmp_block_t struct) in a vector.
+ * 
+ * @param allocation Initial size of vector.
+ * 
+ * @return An allocated and populated cmp_buff_t struct.
+ * 
+ */
 {
     cmp_buff_t* r;
 
@@ -61,8 +116,15 @@ alloc_cmp_buff(int allocation)
     return r;
 }
 
+
 void
 grow_cmp_buff(cmp_buff_t** cmp_buff)
+/**
+ * @brief Grows a cmp_buff_t struct. Deallocated old cmp_buff_t and replaces in place with new pointer.
+ * 
+ * @param cmp_buff A dereferenced pointer to desired cmp_buff_t to grow. New cmp_buff_t will be located at the same addresss.
+ * 
+ */
 {
     cmp_buff_t* new_cmp_buff;
     int i = 0;
@@ -81,8 +143,17 @@ grow_cmp_buff(cmp_buff_t** cmp_buff)
     return;
 }
 
+
 void
 append_cmp_block(cmp_buff_t** cmp_buff, cmp_block_t* buff)
+/**
+ * @brief Appends a compressed block to the compressed block vector. Grows vector if needed.
+ * 
+ * @param cmp_buff A dereferenced pointer to cmp_buff_t vector.
+ * 
+ * @param buff Desired compressed block to append to vector.
+ * 
+ */
 {
     if ((*cmp_buff)->populated == (*cmp_buff)->allocated) grow_cmp_buff(cmp_buff);
 
@@ -92,8 +163,16 @@ append_cmp_block(cmp_buff_t** cmp_buff, cmp_block_t* buff)
     return;
 }
 
+
 data_block_t*
 alloc_data_block(size_t max_size)
+/**
+ * @brief Allocates a data_block_t struct.
+ * 
+ * @param max_size The maximum size the data block can store (cmp_blk_size)
+ * 
+ * @return A populated data_block_t struct with capacity max_size and max_size field set.
+ */
 {
     data_block_t* r;
     r = (data_block_t*)malloc(sizeof(data_block_t));
@@ -103,17 +182,37 @@ alloc_data_block(size_t max_size)
     return r;
 }
 
+
 void
 dealloc_data_block(data_block_t* db)
+/**
+ * @brief Deallocates a data_block_struct and its members.
+ * 
+ * @param db An allocated data_block_struct.
+ * 
+ */
 {
-    free(db->mem);
-    free(db);
-
+    if (db)
+    {
+        if (db->mem) free(db->mem);
+        free(db);
+    }
     return;
 }
 
+
 cmp_block_t*
 alloc_cmp_block(char* mem, size_t size)
+/**
+ * @brief Allocates and populates a cmp_block_t struct to store a compressed block.
+ * 
+ * @param mem Contents of compressed block.
+ * 
+ * @param size Length of the compressed block.
+ * 
+ * @returns A populated cmp_block_t struct with contents of compressed block.
+ * 
+ */
 {
     cmp_block_t* r;
     r = (cmp_block_t*)malloc(sizeof(cmp_block_t));
@@ -122,16 +221,30 @@ alloc_cmp_block(char* mem, size_t size)
     return r;
 }
 
-int
-append_mem(data_block_t* data_buff, char* mem, size_t buff_len)
-{
-    if(buff_len+data_buff->size > data_buff->max_size) return 0;
 
-    memcpy(data_buff->mem+data_buff->size, mem, buff_len);
-    data_buff->size += buff_len;
+int
+append_mem(data_block_t* data_block, char* mem, size_t buff_len)
+/**
+ * @brief Appends data to a data block.
+ * 
+ * @param data_block Data block struct to append to.
+ * 
+ * @param mem Desired contents to append.
+ * 
+ * @param buff_len Length of contents to append
+ * 
+ * @return 1 on success, 0 if there is not enough space in data_block to append.
+ * 
+ */
+{
+    if(buff_len+data_block->size > data_block->max_size) return 0;
+
+    memcpy(data_block->mem+data_block->size, mem, buff_len);
+    data_block->size += buff_len;
 
     return 1;
 }
+
 
 void
 cmp_xml_routine(ZSTD_CCtx* czstd,
@@ -142,6 +255,35 @@ cmp_xml_routine(ZSTD_CCtx* czstd,
                 size_t cmp_blk_size,
                 size_t* tot_size,
                 size_t* tot_cmp)
+/**
+ * @brief A routine to compress an XML block.
+ * Given an offset within an .mzML document and length, this function will append the text data to 
+ * a data block until it is full. Once a data block is full, the data block will be compressed using
+ * zstd_compress() and a cmp_block will be allocated, populated, and appened to the cmp_buff. After 
+ * compression, the old data block will be deallocated.
+ * 
+ * @param czstd A ZSTD compression context allocated by alloc_cctx() (one per thread).
+ * 
+ * @param cmp_buff A dereferenced pointer to the cmp_buff vector.
+ * 
+ * @param curr_block Current data block to append to and/or compress.
+ * 
+ * @param input A pointer within the .mzML document to store within a data block.
+ * 
+ * @param len The length of the desired substring to compress within the .mzML document.
+ * 
+ * @param cmp_blk_size The size of a data block before it is compressed. 
+ *                     NOTE: If this value is set too small, the program will exit with error code 1.
+ *                           The current routine does not handle a substring that is too big for a 
+ *                           data block. This should not be an issue with reasonable a reasonable
+ *                           block size (>10 MB). This gives the user the flexbility to test 
+ *                           performance with different block sizes.
+ * 
+ * @param tot_size A pass-by-reference variable to bookkeep total number of XML bytes processed.
+ * 
+ * @param tot_cmp A pass-by-reference variable to bookkeep total compressed size of XML.
+ * 
+ */
 {
     void* cmp;
     cmp_block_t* cmp_block;
@@ -172,6 +314,7 @@ cmp_xml_routine(ZSTD_CCtx* czstd,
     }
 }
 
+
 void
 cmp_xml_flush(ZSTD_CCtx* czstd,
               cmp_buff_t** cmp_buff,
@@ -179,6 +322,28 @@ cmp_xml_flush(ZSTD_CCtx* czstd,
               size_t cmp_blk_size,
               size_t* tot_size,
               size_t* tot_cmp)
+/**
+ * @brief "Flushes" the current data block by compressing and appending to cmp_buff vector.
+ * Handles the remainder of data blocks stored in the cmp_xml_routine that did not fully populate
+ * a data block to be compressed.
+ * 
+ * @param czstd A ZSTD compression context allocated by alloc_cctx() (one per thread).
+ * 
+ * @param cmp_buff A dereferenced pointer to the cmp_buff vector.
+ * 
+ * @param curr_block Current data block to append to and/or compress.
+ * 
+ * @param cmp_blk_size The size of a data block before it is compressed. 
+ *                     NOTE: If this value is set too small, the program will exit with error code 1.
+ *                           The current routine does not handle a substring that is too big for a 
+ *                           data block. This should not be an issue with reasonable a reasonable
+ *                           block size (>10 MB). This gives the user the flexbility to test 
+ *                           performance with different block sizes.
+ * 
+ * @param tot_size A pass-by-reference variable to bookkeep total number of XML bytes processed.
+ * 
+ * @param tot_cmp A pass-by-reference variable to bookkeep total compressed size of XML.
+ */
 {
     void* cmp;
     cmp_block_t* cmp_block;
@@ -201,12 +366,27 @@ cmp_xml_flush(ZSTD_CCtx* czstd,
     append_cmp_block(cmp_buff, cmp_block);
 
     dealloc_data_block(curr_block);
-
-
 }
+
 
 cmp_buff_t*
 compress_xml(char* input_map, data_positions* dp, size_t cmp_blk_size)
+/**
+ * @brief Main function to compress XML data within a .mzML data.
+ * 
+ * @param input_map A mmap pointer to the .mzML file.
+ * 
+ * @param dp A data_positions struct populated by preprocess.c:find_binary()
+ * 
+ * @param cmp_blk_size The size of a data block before it is compressed. 
+ *                     NOTE: If this value is set too small, the program will exit with error code 1.
+ *                           The current routine does not handle a substring that is too big for a 
+ *                           data block. This should not be an issue with reasonable a reasonable
+ *                           block size (>10 MB). This gives the user the flexbility to test 
+ *                           performance with different block sizes.
+ * 
+ * @return A cmp_buff_t struct representing a vector of compressed buffers to be written to disk.
+ */
 {
     ZSTD_CCtx* czstd;
 
@@ -257,6 +437,9 @@ compress_xml(char* input_map, data_positions* dp, size_t cmp_blk_size)
     cmp_xml_flush(czstd, &cmp_buff, curr_block, cmp_blk_size, &tot_size, &tot_cmp); /* Flush remainder datablocks */
 
     printf("\tXML size: %ld Compressed XML size: %ld (%1.2f%%)\n", tot_size, tot_cmp, (double)tot_size/tot_cmp);
+
+    /* Cleanup (curr_block already freed by cmp_xml_flush) */
+    dealloc_cctx(czstd);
 
     return cmp_buff;
 }
