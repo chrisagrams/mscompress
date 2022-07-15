17af0f4ba321c10bdd7c5fc7b3990247066ea1ea
Chris Grams <cgrams2@uic.edu>
2022-07-15 14:53:16 -0500

    encode.c documentation and cleanup

--- src/encode.c
+++ src/encode.c
@@ -1,3 +1,15 @@
+/**
+ * @file encode.c
+ * @author Chris Grams (chrisagrams@gmail.com)
+ * @brief A collection of functions to encode raw binary to base64 and zlib compressed strings. 
+ *        Uses https://github.com/aklomp/base64.git library for base64 encoding.
+ * @version 0.0.1
+ * @date 2021-12-21
+ * 
+ * @copyright 
+ * 
+ */
+
 #include "mscompress.h"
 #include "vendor/base64/include/libbase64.h"
 #include <stdio.h>
@@ -9,6 +21,18 @@
 
 char*
 encode_base64(zlib_block_t* zblk, size_t src_len, size_t* out_len)
+/**
+ * @brief Takes a zlib compressed block (zlib_block_t) and returns a base64 encoded string.
+ * 
+ * @param zblk A zlib_block_t struct with zblk->buff populated.
+ * 
+ * @param src_len Length of original zlib output string.
+ * 
+ * @param out_len Return by reference of output string length.
+ * 
+ * @return A base64 string.
+ * 
+ */
 {
     char* b64_out_buff;
 
@@ -23,6 +47,16 @@ encode_base64(zlib_block_t* zblk, size_t src_len, size_t* out_len)
 
 char*
 encode_binary(char** src, size_t* out_len)
+/**
+ * @brief Takes in a raw binary string, returns a zlib compressed and base64 encoded string.
+ * 
+ * @param src Pointer to source string. src pointer will point to the end of the string on return.
+ * 
+ * @param out_len Return by reference of output string length.
+ * 
+ * @return A zlib compressed and base64 encoded string.
+ * 
+ */
 {
     Bytef* zlib_encoded;
 
@@ -31,13 +65,6 @@ encode_binary(char** src, size_t* out_len)
     zlib_block_t* decmp_input;
 
     zlib_block_t* cmp_output;
-
-    // if(**src == '\0')
-    // {
-    //     fprintf(stderr, "encode_binary received NULL\n");
-    //     // exit(1);
-    //     return NULL;
-    // }
 
     decmp_input = zlib_alloc(ZLIB_SIZE_OFFSET);
     decmp_input->mem = *src;
@@ -48,7 +75,7 @@ encode_binary(char** src, size_t* out_len)
 
     uint16_t org_len = *(uint16_t*)decmp_header;
 
-    zlib_len = (size_t)zlib_compress((Bytef*)*src+ZLIB_SIZE_OFFSET, cmp_output, org_len);
+    zlib_len = (size_t)zlib_compress(((Bytef*)*src) + ZLIB_SIZE_OFFSET, cmp_output, org_len);
 
     *src += (ZLIB_SIZE_OFFSET + org_len);
 
