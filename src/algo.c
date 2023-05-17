59330bf892ae0fcd6220f513774a690e34b7e7d3
cgrams <cgrams2@uic.edu>
2023-05-17 11:01:22 -0500

    delta fixes

--- src/algo.c
+++ src/algo.c
@@ -427,13 +427,15 @@ algo_decode_delta32_transform_64d (void* args)
     // *(float*)&res[0] = f[0];
     memcpy(tmp, f, sizeof(double));
 
+    tmp = (uint32_t*)((void*)tmp + sizeof(double));
+
     // Perform delta transform
     for(int i = 1; i < len; i++)
     {
         float diff = f[i] - f[i-1];
         // uint16_t uint_diff = (diff > 0) ? (uint16_t)floor(diff) : 0; // clamp to 0 if diff is negative
         uint32_t uint_diff = (uint32_t)floor(diff * DELTA_SCALE_FACTOR); // scale by 2^16 / 10
-        tmp[i] = uint_diff;
+        tmp[i-1] = uint_diff;
     }
 
     // Free decoded buffer
@@ -682,7 +684,7 @@ algo_encode_delta16_transform_32f (void* args)
     // Perform delta transform
     res[0] = start;
     for(size_t i = 1; i < len; i++)
-        res[i] = res[i-1] + ((float)arr[i] / DELTA_SCALE_FACTOR);
+        res[i] = res[i-1] + ((float)arr[i-1] / DELTA_SCALE_FACTOR);
     
     // Encode using specified encoding format
     a_args->enc_fun(a_args->z, (char**)(&res), res_len, a_args->dest, a_args->dest_len);
@@ -735,7 +737,7 @@ algo_encode_delta16_transform_64d (void* args)
     // Perform delta transform
     res[0] = start;
     for(size_t i = 1; i < len; i++)
-        res[i] = res[i-1] + ((double)arr[i] / DELTA_SCALE_FACTOR);
+        res[i] = res[i-1] + ((double)arr[i-1] / DELTA_SCALE_FACTOR);
     
     // Encode using specified encoding format
     a_args->enc_fun(a_args->z, (char**)(&res), res_len, a_args->dest, a_args->dest_len);
@@ -788,7 +790,7 @@ algo_encode_delta32_transform_32f (void* args)
     // Perform delta transform
     res[0] = start;
     for(size_t i = 1; i < len; i++)
-        res[i] = res[i-1] + ((float)arr[i] / DELTA_SCALE_FACTOR);
+        res[i] = res[i-1] + ((float)arr[i-1] / DELTA_SCALE_FACTOR);
     
     // Encode using specified encoding format
     a_args->enc_fun(a_args->z, (char**)(&res), res_len, a_args->dest, a_args->dest_len);
@@ -841,7 +843,7 @@ algo_encode_delta32_transform_64d (void* args)
     // Perform delta transform
     res[0] = start;
     for(size_t i = 1; i < len; i++)
-        res[i] = res[i-1] + ((double)arr[i] / DELTA_SCALE_FACTOR);
+        res[i] = res[i-1] + ((double)arr[i-1] / DELTA_SCALE_FACTOR);
     
     // Encode using specified encoding format
     a_args->enc_fun(a_args->z, (char**)(&res), res_len, a_args->dest, a_args->dest_len);
