ec972b4a7fefcedd593d1daaa9904954d9113b17
cgrams <cgrams2@uic.edu>
2023-06-06 11:27:09 -0500

    initialize int_scale_factor

--- src/mscompress.c
+++ src/mscompress.c
@@ -72,20 +72,21 @@ static void validate_algo_name(const char* name) {
 static void 
 parse_arguments(int argc, char* argv[], struct Arguments* arguments) {
   int i;
-  arguments->verbose         = 0;
-  arguments->threads         = 0;
-  arguments->extract_only    = 0;
-  arguments->mz_lossy        = NULL;
-  arguments->int_lossy       = NULL;
-  arguments->blocksize       = 1e+8;
-  arguments->input_file      = NULL;
-  arguments->output_file     = NULL;
-  arguments->mz_scale_factor = 1000; // initialize scale factor to default value
-  arguments->indices         = NULL;
-  arguments->indices_length  = 0;
-  arguments->scans           = NULL;
-  arguments->scans_length    = 0;
-  arguments->ms_level        = 0;
+  arguments->verbose          = 0;
+  arguments->threads          = 0;
+  arguments->extract_only     = 0;
+  arguments->mz_lossy         = NULL;
+  arguments->int_lossy        = NULL;
+  arguments->blocksize        = 1e+8;
+  arguments->input_file       = NULL;
+  arguments->output_file      = NULL;
+  arguments->mz_scale_factor  = 1000; // initialize scale factor to default value
+  arguments->int_scale_factor = 0;
+  arguments->indices          = NULL;
+  arguments->indices_length   = 0;
+  arguments->scans            = NULL;
+  arguments->scans_length     = 0;
+  arguments->ms_level         = 0;
 
   arguments->target_xml_format   = _ZSTD_compression_; // default
   arguments->target_mz_format    = _ZSTD_compression_; // default
