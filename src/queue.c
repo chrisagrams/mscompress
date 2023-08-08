d3c00150a89e6fb8704fffff27d9c8cd396553c9
cgrams <cgrams2@uic.edu>
2023-08-08 15:35:13 -0500

    read_block_len_queue fix

--- src/queue.c
+++ src/queue.c
@@ -281,8 +281,7 @@ read_block_len_queue(void* input_map, long offset, long end)
 
     factor = sizeof(size_t)*2;
 
-    char* input_ptr = (char*)input_map;
-
+    char* input_ptr = (char*)(input_map + offset);
 
     for(i = 0; i < diff; i+=factor)
         append_block_len(r, *(size_t*)(input_ptr+i), *(size_t*)(input_ptr+i+sizeof(size_t)));
