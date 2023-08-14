e456b3982fb7fbfc6f7f2fd2da517437f83051e3
cgrams <cgrams2@uic.edu>
2023-08-14 13:23:01 -0500

    minor test fix

--- cli/test/lossless_tests/run_test.sh
+++ cli/test/lossless_tests/run_test.sh
@@ -3,8 +3,8 @@
 for i in *.mzML; do
     tput sgr0;
     echo "Testing $i..."
-    ../../mscompress --threads=1 "$i" ./test.msz
-    ../../mscompress --threads=1 ./test.msz ./test.mzML
+    ../../mscompress --threads 1 "$i" ./test.msz
+    ../../mscompress --threads 1 ./test.msz ./test.mzML
     python3 ../validate.py "$i" ./test.mzML 0 0
     if [ $? -eq 0 ]; then
         tput setab 2; echo "Lossless test $i passed"; tput sgr0;
--- cli/test/lossy_log_tests/run_test.sh
+++ cli/test/lossy_log_tests/run_test.sh
@@ -3,8 +3,8 @@
 for i in *.mzML; do
     tput sgr0;
     echo "Testing $i..."
-    ../../mscompress --threads=1 --lossy=log "$i" ./test.msz
-    ../../mscompress --threads=1 --lossy=log ./test.msz ./test.mzML
+    ../../mscompress --threads 1 --lossy log "$i" ./test.msz
+    ../../mscompress --threads 1 --lossy log ./test.msz ./test.mzML
     python3 ../validate.py "$i" ./test.mzML 0.1 0.1
     if [ $? -eq 0 ]; then
         tput setab 2; echo "Lossy log test $i passed"; tput sgr0;
