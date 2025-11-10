b268247fbf45e54aecebf610a77177976a2befc8
Chris Grams <37988265+chrisagrams@users.noreply.github.com>
2025-11-10 11:25:09 -0600

    bump version

--- src/mscompress.h
+++ src/mscompress.h
@@ -7,7 +7,7 @@
 #include "../vendor/zlib/zlib.h"
 #include "../vendor/zstd/lib/zstd.h"
 
-#define VERSION "1.0.1"
+#define VERSION "1.0.2"
 #define STATUS "Dev"
 #define MIN_SUPPORT "0.1"
 #define MAX_SUPPORT "0.1"
