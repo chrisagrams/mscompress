1f32a0474696e593b176e4ba8717618cf6ff02b0
Chris Grams <chrisagrams@gmail.com>
2026-02-18 11:13:30 -0600

    switch to prebuild

--- node-ts/scripts/build.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import { execSync } from "child_process";
-
-// Use "Unix Makefiles" on non-Windows to avoid Ninja, which doesn't
-// support the add_custom_target approach used by ZlibInclude.cmake.
-const generator = process.platform === "win32" ? "" : '-G "Unix Makefiles"';
-execSync(`cmake-js compile ${generator}`.trim(), { stdio: "inherit" });
